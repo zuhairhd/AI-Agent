eeedbce041325bd18fdbe34f4e0fd5b846aaecd8
zuhairhd <zuhairhd@gmail.com>
2026-04-12 10:54:37 +0000

    fix static entry of email should use vlues in .env

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,15 +2,26 @@ from .base import *
 import os
 
 # Email (Gmail SMTP)
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend")
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.gmail.com")
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True").lower() == "true"
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
+DEFAULT_FROM_EMAIL = os.environ.get(
+    "DEFAULT_FROM_EMAIL",
+    f"Future Smart Support <{EMAIL_HOST_USER}>"
+)
 
-EMAIL_HOST_USER = "zuhairhd@gmail.com"
-EMAIL_HOST_PASSWORD = "ryjftibdagwjarsi"
+#EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+#EMAIL_HOST = "smtp.gmail.com"
+#EMAIL_PORT = 587
+#EMAIL_USE_TLS = True
 
-DEFAULT_FROM_EMAIL = "Future Smart Support <zuhairhd@gmail.com>"
+#EMAIL_HOST_USER = "zuhairhd@gmail.com"
+#EMAIL_HOST_PASSWORD = "ryjftibdagwjarsi"
+
+#DEFAULT_FROM_EMAIL = "Future Smart Support <zuhairhd@gmail.com>"
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
 
 
